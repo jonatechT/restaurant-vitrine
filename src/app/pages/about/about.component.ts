91a3526c91eeb48d4e3eb5b353b7b1a1959ea603
jonatechT <226amiral@gmail.com>
2026-05-11 15:36:49 -0700

    Amélioration page about + ajout Lucide Icons + redesign UI

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {
+  LucideAngularModule,
+  ChefHat,
+  Flame,
+  Star,
+  Phone,
+  MapPin,
+  Clock3,
+  UtensilsCrossed,
+} from 'lucide-angular';
+
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    LucideAngularModule
+  ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {}
+export class AboutComponent {
+
+  readonly ChefHat = ChefHat;
+  readonly Flame = Flame;
+  readonly Star = Star;
+  readonly Phone = Phone;
+  readonly MapPin = MapPin;
+  readonly Clock3 = Clock3;
+  readonly UtensilsCrossed = UtensilsCrossed;
+}
